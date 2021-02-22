d51d8c705ca2ee3d29e878274094ee731927c12c
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-22 16:10:11 -0300

    176.Leer argumentos por URL

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroesById } from '../../selectors/getHeroesById';
 
 export const HeroScreen = () => {
+
+    const { heroId } = useParams();
+    
+    const hero = getHeroesById( heroId );
+
+    if ( !hero ) {
+        return <Redirect to="/" />
+    }
+
+    const {
+        publisher,
+        superhero,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+
     return (
         <div>
             <h1>Hero Screen</h1>
