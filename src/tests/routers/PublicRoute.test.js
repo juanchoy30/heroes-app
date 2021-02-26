07ac2daf5ecaadd791ad57e4aa153b902df4868b
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-26 10:41:31 -0300

    Extra. Tests on Public Route

--- src/tests/routers/PublicRoute.test.js
+++ src/tests/routers/PublicRoute.test.js
@@ -2,56 +2,51 @@ import React from 'react';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // *1
 import { mount, configure } from 'enzyme'; // *1
 import { MemoryRouter } from 'react-router-dom';
-import { PrivateRoute } from '../../routers/PrivateRoute';
+import { PublicRoute } from '../../routers/PublicRoute';
 
 configure({adapter: new Adapter()}); 
 
 // *1 This feature was not in the course. I found it on stackOverFlow, It seems to be
 // a problem with the wojtekmaj adapter patch for enzima-react17. 
 
-describe('<PrivateRoute /> Tests', () => {
-
+describe('<PublicRoute /> Tests', () => {
+    
     const props = {
         location: {
-            pathname: '/marvel'
+            pathname: '/login'
         }
     }
 
-    Storage.prototype.setItem = jest.fn();
-    
-    test('should show the component if authenitcated, and then save in localStorage', () => {
+    test('should show the component if not authenticated', () => {
         
         const wrapper = mount(
             <MemoryRouter>
-                <PrivateRoute
-                    isAuthenticated={ true }
-                    component={ () => <span>Ready!</span> }
+                <PublicRoute 
+                    isAuthenticated={ false }
+                    component={ () => <span>Not looged, it should be shown</span> }
                     { ...props }
                 />
             </MemoryRouter>
         );
 
         expect( wrapper.find('span').exists() ).toBe( true );
-        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel');
 
     });
-    
 
-    test('should block the component if not authenticated', () => {
+    test('should block the component if authenticated', () => {
         
         const wrapper = mount(
             <MemoryRouter>
-                <PrivateRoute 
-                    isAuthenticated={ false }
-                    component={ () => <span>It should not appear!</span> }
+                <PublicRoute 
+                    isAuthenticated={ true }
+                    component={ () => <span>Not looged, it should be shown</span> }
                     { ...props }
                 />
             </MemoryRouter>
         );
 
         expect( wrapper.find('span').exists() ).toBe( false );
-        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel');
-
+        
     });
     
 })
